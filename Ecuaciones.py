168ef89ba184eb4679d9f159aef7aa3a8a09b054
Qhtl9 <gabriel.ortiz5010@gmail.com>
2023-11-24 00:55:02 -0600

    Interfaz gráfica agregada

--- Ecuaciones.py
+++ Ecuaciones.py
@@ -1,7 +1,10 @@
-import math # Para las raíces
-import matplotlib.pyplot as plt # Para los gráficos
-from tabulate import tabulate # Para las tablas
+import math
+import matplotlib.pyplot as plt
+from tabulate import tabulate
+import tkinter as tk
+from tkinter import Label, Entry, Button, Text, scrolledtext
 
+# Coeficientes para el cálculo de Tsat
 n1 = 0.116_705_214_527_67E4
 n2 = -0.724_213_167_032_06E6
 n3 = -0.170_738_469_400_92E2
@@ -13,110 +16,140 @@ n8 = 0.405_113_405_420_57E6
 n9 = -0.238_555_575_678_49
 n10 = 0.650_175_348_447_98E3
 
-C_ea = 4186 # [J/(kg°C)]
+C_ea = 4186  # [J/(kg°C)]
 
-def Cálculo_Tsat(p): # p: Presión en [MPa]
+# Funciones de cálculo
+def calculo_Tsat(p):
     beta = pow(p, 0.25)
-    E = (pow(beta, 2)) + (n3*beta) + n6
-    F = (n1*pow(beta, 2)) + (n4*beta) + n7
-    G = (n2*pow(beta, 2)) + (n5*beta) + n8
-    D = (2*G)/(-F-math.sqrt(pow(F, 2)-4*E*G))
-    T_sat = ((n10 + D - math.sqrt(pow((n10 + D), 2)-4*(n9 + n10*D)))/2.0) - 273.15
+    E = (pow(beta, 2)) + (n3 * beta) + n6
+    F = (n1 * pow(beta, 2)) + (n4 * beta) + n7
+    G = (n2 * pow(beta, 2)) + (n5 * beta) + n8
+    D = (2 * G) / (-F - math.sqrt(pow(F, 2) - 4 * E * G))
+    T_sat = ((n10 + D - math.sqrt(pow((n10 + D), 2) - 4 * (n9 + n10 * D))) / 2.0) - 273.15
+    return T_sat  # [°C]
 
-    return T_sat # [°C]
+def calculo_entalpia_vap(T_sat):
+    h_fg = 2256.4 * (pow(((1 - ((T_sat + 273.15) / 647.096)) / (1 - 0.57665623)), 0.375))
+    return h_fg  # [kJ/kg]
 
-def Cálculo_EntalpíaVap(T_sat): # Tsat: Temperatura de saturación en [°C]
-    h_fg = 2256.4*(pow(((1-((T_sat+273.15)/(647.096)))/(1-0.57665623)), 0.375))
-    
-    return h_fg # [kJ/kg]
+def calculo_calidad(Valor, Valor_l, Valor_v):
+    return ((Valor - Valor_l) / (Valor_v - Valor_l))
 
-def Cálculo_Calidad(Valor, Valor_l, Valor_v):
-    return ((Valor - Valor_l)/(Valor_v - Valor_l))
-
-# Calcular la temperatura y la masa líquida en el tiempo dado
-def Cáculo_MasaTemperatura(C_ea, T_i, M_i, P, T_sat, t_sat, t_vap, t):
-    if(t_sat >= t):
+def calculo_masa_temperatura(C_ea, T_i, M_i, P, T_sat, t_sat, t_vap, t):
+    if t_sat >= t:
         M_t = M_i
-        Q_t = P*t
-        T_t = (Q_t*1/(M_t*C_ea)) + T_i
-    elif(t_vap >= t):
-        x = Cálculo_Calidad(t, t_vap+t_sat, t_sat)
-        M_t = M_i*x
-        Q_t = P*t
+        Q_t = P * t
+        T_t = (Q_t * 1 / (M_t * C_ea)) + T_i
+    elif t_vap >= t:
+        x = calculo_calidad(t, t_vap + t_sat, t_sat)
+        M_t = M_i * x
+        Q_t = P * t
         T_t = T_sat
     else:
-        return -1;
-    
+        return -1
     return [M_t, T_t]
 
-# Entrada de datos del usuario
-T_i = float(input("Introduzca la temperatura inicial del agua\n\t[°C]> "))
-M_i = float(input("Introduzca la masa inicial del agua\n\t[kg]> "))
-t = float(input("Introduzca el intervalo de tiempo para obtener la temperatura y la masa líquida del sistema\n\t[s]> "))
-P = float(input("Introduzca la potencia de la parrilla\n\t[W]> "))
-P_atm = float(input("Introduzca la presión atmosférica\n\t[MPa]> "))
+# Función para manejar el botón "Calcular"
+def calcular():
+    T_i = float(entry_temp_inicial.get())
+    M_i = float(entry_masa_inicial.get())
+    t = float(entry_intervalo_tiempo.get())
+    P = float(entry_potencia_parrilla.get())
+    P_atm = float(entry_presion_atmosferica.get())
 
-# Verificar que la temperatura inicial corresponde a la fase líquida
-T_sat = Cálculo_Tsat(P_atm) # [°C]
-if(T_sat > T_i):
-    print("")
-else:
-    print("No se inicia en fase líquida, saliendo...\n")
-    exit()
+    T_sat = calculo_Tsat(P_atm)  # [°C]
 
-# Calcular el tiempo necesario para llegar a la temperatura de saturación
-Q_a = M_i*C_ea*(T_sat-T_i) # [J]
-t_sat = (1/P)*Q_a # [s]
-
-# Calcular el tiempo necesario para evaporar toda la masa
-h_fg = Cálculo_EntalpíaVap(T_sat) # [kJ/kg]
-h_fg = h_fg*(1000) # [J/kg]
-Q_e = h_fg*M_i # [J]
-t_vap = (1/P)*Q_e # [s]
-
-incremento = 0
-dt = 1 # Resolución de las gráficas, 1 = [s], 0.001 [ms]
-M_ot = []
-T_ot = []
-
-flag = True # Desde una perspectiva de aquitectura, esto es terrible
-while(flag):
-    Data = Cáculo_MasaTemperatura(C_ea, T_i, M_i, P, T_sat, t_sat, t_vap, incremento)
-    if(Data == -1):
-        flag = False
+    if T_sat > T_i:
+        text_resultado.delete(1.0, tk.END)
     else:
-        incremento = incremento + dt
-        M_ot.append(Data[0])
-        T_ot.append(Data[1])
+        text_resultado.delete(1.0, tk.END)
+        text_resultado.insert(tk.END, "No se inicia en fase líquida, saliendo...\n")
+        return
 
-Data_ut = Cáculo_MasaTemperatura(C_ea, T_i, M_i, P, T_sat, t_sat, t_vap, t)
+    Q_a = M_i * C_ea * (T_sat - T_i)  # [J]
+    t_sat = (1 / P) * Q_a  # [s]
 
-tabla_data = [
-    ["Masa de líquido", "Temperatura"],
-    Data_ut
-]
+    h_fg = calculo_entalpia_vap(T_sat)  # [kJ/kg]
+    h_fg = h_fg * (1000)  # [J/kg]
+    Q_e = h_fg * M_i  # [J]
+    t_vap = (1 / P) * Q_e  # [s]
 
-# Crear la tabla utilizando tabulate
-print("Tabla de valores a los {t}[s] de inicio".format(t = t))
-tabla = tabulate(tabla_data, headers="firstrow", tablefmt="grid")
+    incremento = 0
+    dt = 1
+    M_ot = []
+    T_ot = []
 
-# Imprimir la tabla
-print(tabla)
+    flag = True
+    while flag:
+        Data = calculo_masa_temperatura(C_ea, T_i, M_i, P, T_sat, t_sat, t_vap, incremento)
+        if Data == -1:
+            flag = False
+        else:
+            incremento = incremento + dt
+            M_ot.append(Data[0])
+            T_ot.append(Data[1])
 
-# Graficar la lista M_ot
-plt.figure(1)
-plt.plot(M_ot, label='Masa [kg]')
-plt.xlabel('Segundos [s]')
-plt.ylabel('Masa [kg]')
-plt.title('Gráfica de masa líquida en función del tiempo')
-plt.legend()
+    Data_ut = calculo_masa_temperatura(C_ea, T_i, M_i, P, T_sat, t_sat, t_vap, t)
 
-# Graficar la lista T_ot
-plt.figure(2)
-plt.plot(T_ot, label='Temperatura')
-plt.xlabel('Segundos [s]')
-plt.ylabel('Temperatura [ºC]')
-plt.title('Gráfica de temperatura en función del tiempo')
-plt.legend()
+    # Crear tabla
+    tabla_data = [["Masa de líquido", "Temperatura"], Data_ut]
+    tabla_resultado = tabulate(tabla_data, headers="firstrow", tablefmt="grid")
 
-plt.show()
+    # Mostrar tabla
+    text_resultado.delete(1.0, tk.END)
+    text_resultado.insert(tk.END, "Tabla de valores a los {t}[s] de inicio\n".format(t=t))
+    text_resultado.insert(tk.END, tabla_resultado)
+
+    # Graficar M_ot
+    plt.figure(1)
+    plt.plot(M_ot, label='Masa [kg]')
+    plt.xlabel('Segundos [s]')
+    plt.ylabel('Masa [kg]')
+    plt.title('Gráfica de masa líquida en función del tiempo')
+    plt.legend()
+
+    # Graficar T_ot
+    plt.figure(2)
+    plt.plot(T_ot, label='Temperatura')
+    plt.xlabel('Segundos [s]')
+    plt.ylabel('Temperatura [ºC]')
+    plt.title('Gráfica de temperatura en función del tiempo')
+    plt.legend()
+
+    plt.show()
+
+# Crear la interfaz gráfica
+window = tk.Tk()
+window.title("Cálculo de Masa y Temperatura")
+
+# Etiquetas y campos de entrada
+Label(window, text="Temperatura Inicial (°C):").grid(row=0, column=0, padx=10, pady=5, sticky="w")
+entry_temp_inicial = Entry(window)
+entry_temp_inicial.grid(row=0, column=1, padx=10, pady=5)
+
+Label(window, text="Masa Inicial (kg):").grid(row=1, column=0, padx=10, pady=5, sticky="w")
+entry_masa_inicial = Entry(window)
+entry_masa_inicial.grid(row=1, column=1, padx=10, pady=5)
+
+Label(window, text="Intervalo de Tiempo (s):").grid(row=2, column=0, padx=10, pady=5, sticky="w")
+entry_intervalo_tiempo = Entry(window)
+entry_intervalo_tiempo.grid(row=2, column=1, padx=10, pady=5)
+
+Label(window, text="Potencia de la Parrilla (W):").grid(row=3, column=0, padx=10, pady=5, sticky="w")
+entry_potencia_parrilla = Entry(window)
+entry_potencia_parrilla.grid(row=3, column=1, padx=10, pady=5)
+
+Label(window, text="Presión Atmosférica (MPa):").grid(row=4, column=0, padx=10, pady=5, sticky="w")
+entry_presion_atmosferica = Entry(window)
+entry_presion_atmosferica.grid(row=4, column=1, padx=10, pady=5)
+
+# Botón de cálculo
+btn_calcular = Button(window, text="Calcular", command=calcular)
+btn_calcular.grid(row=5, column=0, columnspan=2, pady=10)
+
+# Resultado
+Label(window, text="Resultado:").grid(row=6, column=0, padx=10, pady=5, sticky="w")
+text_resultado = scrolledtext.ScrolledText(window, width=40, height=10, wrap=tk.WORD)
+text_resultado.grid(row=7, column=0, columnspan=2, padx=10, pady=5)
+
+window.mainloop()
